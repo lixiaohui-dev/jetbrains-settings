bdbdca784730dc77bee7f1af4953a5eda144fcfd
Administrator <Administrator@DESKTOP-7L2F3AL>
2021-09-16 16:12:10 +0800

    PY-2020.3.5 <Administrator@DESKTOP-7L2F3AL Create debugger.xml, rInterpreterSettings.xml, colors.scheme.xml, databaseDrivers.xml, updates.xml, _@user_Hiberbee Dark.icls, rainbow_brackets.xml, keymap.xml, editor.xml, Default.xml, ide.general.xml, _@user_Monokai.icls, IntelliLang.xml, Python Script.py, wsl.distributions.xml, pluginAdvertiser.xml, laf.xml, ui.lnf.xml, Windows copy.xml, ide-features-trainer.xml, _@user_Xcode-Dark.icls, Default.xml, rPackageDescription.xml, jdk.table.xml, proxy.settings.xml, notifications.xml, filetypes.xml, find.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Created by: 李晓辉
+# Created on: ${DATE}
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime, timedelta
+pd.options.mode.chained_assignment = None  # default='warn'
