0796e2acb5a8c2a0e1fe8a8ba0f4fdfc1483739f
lixiaohui <lixiaohui@192.168.1.3>
2022-05-14 17:57:26 +0800

    PC-2021.3.2 <lixiaohui@mac Overwrite remote https://github.com/lixiaohui-dev/jetbrains-settings.git to local

--- fileTemplates/internal/Python Script.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Created by: 李晓辉
-# Created on: ${DATE}
-import pandas as pd
-import numpy as np
-import os
-from datetime import datetime, timedelta
-pd.options.mode.chained_assignment = None  # default='warn'
